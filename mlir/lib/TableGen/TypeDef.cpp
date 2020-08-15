62935b167c6ec1d9fa8c52e0f4fd7471fee9f914
John Demme <john.demme@microsoft.com>
2020-08-15 02:12:24 +0000

    Add TypeDef td, test, and wrapper classes (untested)

---
+++ mlir/lib/TableGen/TypeDef.cpp
@@ -0,0 +1,61 @@
+//===- TypeDef.cpp - TypeDef wrapper class --------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// TypeDef wrapper to simplify using TableGen Record defining a MLIR dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/TableGen/TypeDef.h"
+#include "llvm/TableGen/Record.h"
+
+using namespace mlir;
+using namespace mlir::tblgen;
+
+StringRef TypeDef::getName() const { return def->getValueAsString("name"); }
+
+StringRef TypeDef::getStorageClassName() const { return def->getValueAsString("storageClass"); }
+StringRef TypeDef::getStorageNamespace() const { return def->getValueAsString("storageNamespace"); }
+
+bool TypeDef::genStorageClass() const{
+  return def->getValueAsBit("genStorageClass");
+}
+bool TypeDef::hasStorageCustomConstructor() const{
+  return def->getValueAsBit("hasStorageCustomConstructor");
+}
+llvm::Optional<ArrayRef<TypeMember>> TypeDef::getMembers() const{
+  // auto membersDag = def->getValueAsDag("members");
+  return ArrayRef<TypeMember>();
+}
+StringRef TypeDef::getMnemonic() const{
+  return def->getValueAsString("mnemonic");
+}
+StringRef TypeDef::getPrinterCode() const{
+  return def->getValueAsString("printer");
+}
+StringRef TypeDef::getParserCode() const{
+  return def->getValueAsString("parser");
+}
+bool TypeDef::genAccessors() const{
+  return def->getValueAsBit("genAccessors");
+}
+bool TypeDef::genVerifyInvariantsDecl() const{
+  return def->getValueAsBit("genVerifyInvariantsDecl");
+}
+
+llvm::Optional<StringRef> TypeDef::getExtraDecls() const {
+  auto value = def->getValueAsString("extraDecls");
+  return value.empty() ? llvm::Optional<StringRef>() : value;
+}
+
+bool TypeDef::operator==(const TypeDef &other) const {
+  return def == other.def;
+}
+
+bool TypeDef::operator<(const TypeDef &other) const {
+  return getName() < other.getName();
+}
